67cf48b1b6da4325cea972875ffdcb1295d2e98e
Rodrigo Amaral <rodrigo.amaral94@hotmail.com>
2024-10-14 22:37:59 -0300

    cards home

--- src/components/home/deals-chart.tsx
+++ src/components/home/deals-chart.tsx
@@ -5,33 +5,53 @@ import { Area, AreaConfig } from "@ant-design/plots"
 import { useList } from "@refinedev/core"
 import { DASHBOARD_DEALS_CHART_QUERY } from "../../graphql/queries"
 import React from "react"
+import { mapDealsData } from "../../utilities"
+import { GetFieldsFromList } from "@refinedev/nestjs-query"
+import { DashboardDealsChartQuery } from "../../graphql/types"
 const DealsChart = () => {
 
-    // const { data } = useList({
-    //     resource: 'dealsStages',
-    //     meta: {
-    //         gqlQuery: DASHBOARD_DEALS_CHART_QUERY
-    //     }
-    // })
-    // const dealData = React.useMemo(() => {
-    //     // return mapDealsData(data?.data);
-    //     return data?.data
-    // }, [data?.data])
+    const { data } = useList<GetFieldsFromList<DashboardDealsChartQuery>>({
+        resource: 'dealStages',
+        filters: [{
+            field: 'title', operator: 'in', value: ['WON', 'LOST']
+        }],
+        meta: {
+            gqlQuery: DASHBOARD_DEALS_CHART_QUERY
+        }
+    })
+    const dealData = React.useMemo(() => {
+        return mapDealsData(data?.data);
+    }, [data?.data])
+
     const config: AreaConfig = {
-        data: {
-            type: 'fetch',
-            value: 'https://assets.antv.antgroup.com/g2/aapl.json',
-        },
-        xField: (d) => new Date(d.date),
-        yField: 'close',
-        stack: false,
+        data: dealData,
+        xField: 'timeText',
+        yField: 'value',
+        isStack: false,
         seriesField: 'state',
         animation: true,
         startOnZero: false,
         smooth: true,
         legend: {
             offsetY: -6
+        },
+        yAxis: {
+            tickCount: 4,
+            label: {
+                formatter: (v: string) => {
+                    return `$${Number(v) / 1000}k`
+                }
+            }
+        },
+        tooltip: {
+            formatter: (data) => {
+                return {
+                    name: data.state,
+                    value: `$${Number(data.value) / 1000}k`
+                }
+            }
         }
+
     }
     return (
 
