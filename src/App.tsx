888b766cc2eab0b1fc38ce9c9da0ca6ec28c3de3
Rodrigo Amaral <rodrigo.amaral94@hotmail.com>
2024-10-17 19:51:20 -0300

    create company

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,8 @@ import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import { Home, ForgotPassword, Login, Register } from "./pages"
 import Layout from "./components/layout";
 import { resources } from "./config/resources";
+import CompanyList from "./pages/company/list";
+import Create from "./pages/company/create";
 
 function App() {
   return (
@@ -50,6 +52,10 @@ function App() {
                 </Authenticated>
                 } >
                   <Route index element={<Home />} />
+                  <Route path="/companies" >
+                    <Route index element={<CompanyList />} />
+                    <Route path="new" element={<Create />} />
+                  </Route>
 
                 </Route>
               </Routes>
